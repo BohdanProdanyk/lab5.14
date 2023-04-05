45a5ade4644e94ca89248c244019ac3e3d22b819
BohdanProdanyk <126155459+BohdanProdanyk@users.noreply.github.com>
2023-04-06 01:04:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //task 1
+
+//        Scanner x = new Scanner(System.in);
+//        String q, w, e, r, t, str;
+//        System.out.println("Введіть рандомне слово:");
+//        q = x.nextLine();
+//        w = x.nextLine();
+//        e = x.nextLine();
+//        r = x.nextLine();
+//        t = x.nextLine();
+//        str = q + " ".concat(w + " ".concat(e + " ".concat(r + " ".concat(t))));
+//        System.out.println(str);
+
+
+        //task 2
+//        String w1, e1, r1, t1, y1, str1;
+//        System.out.println("Введіть рандомне слово:");
+//        w1 = x.nextLine();
+//        e1 = x.nextLine();
+//        r1 = x.nextLine();
+//        t1 = x.nextLine();
+//        y1 = x.nextLine();
+//        str1 = w.substring(0,1) + " " + e.substring(0,1) + " " + r.substring(0,1) + " " + t.substring(0,1) + " " + y.substring(0,1) ;
+//        System.out.println(str1);
+
+
+
+//        //task 3
+//        double q2, w2, e2;
+//        Scanner x1 = new Scanner(System.in);
+//        System.out.print("Введіть дробове число:");
+//        if (x1.hasNextDouble()){
+//            q2 = x1.nextDouble();
+//        }else{
+//            System.out.println("Помилка");
+//            return;
+//        }
+//        if (x1.hasNextDouble()){
+//            w2 = x1.nextDouble();
+//        }else{
+//            System.out.println("Помилка");
+//            return;
+//        }
+//        if (x1.hasNextDouble()){
+//            e2 = x1.nextDouble();
+//        }else{
+//            System.out.println("Помилка");
+//            return;
+//        }
+//            if (q2 >= w2 & q2 >= e2) {
+//                System.out.println("Найбільше число:" + q2);
+//            } else if (w2 >= q2 & w2 >= e2) {
+//                System.out.println("Найбільше число:" + w2);
+//            } else {
+//                System.out.println("Найбільше число:" + e2);
+//            }
+
+
+        //task 4
+//        String q3, w3;
+//        Scanner x2 = new Scanner(System.in);
+//        System.out.print("Введіть ім'я:");
+//        q3 = x2.nextLine();
+//        w3 = x2.nextLine();
+//        System.out.println(q3.equals(w3));
+
+
+        //task 5
+//        String q4, w4;
+//        Scanner x3 = new Scanner(System.in);
+//        System.out.print("Введіть ім'я:");
+//        q4 = x3.nextLine();
+//        w4 = x3.nextLine();
+//        int p = q4.length();
+//        int l = w4.length();
+//        if(p > l){
+//            System.out.println("Перше ім'я більще за інше");
+//        }
+//        else if (p < l) {
+//            System.out.println("Перше ім'я менше за інше");
+//        }
+//        else {
+//            System.out.println("Перше ім'я одинакове за пропорціями з іншим");
+//        }
+
+
+    }
+}
